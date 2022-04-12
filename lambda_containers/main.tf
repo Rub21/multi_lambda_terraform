875ea6923908b921531c8dd4d54ffe2131ba8aab
Rub21 <ruben@developmentseed.org>
2022-04-11 21:19:57 -0500

    Add tempaltee for lambda - image container

--- lambda_containers/main.tf
+++ lambda_containers/main.tf
@@ -53,6 +53,22 @@ resource "aws_lambda_function" "lambda_functions" {
   depends_on = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
 
+data "aws_lambda_invocation" "lambda_functions_invocation" {
+  function_name = aws_lambda_function.lambda_function_test.function_name
+  input = <<JSON
+    {
+      "key1": "value1",
+      "key2": "value2"
+    }
+    JSON
+}
+
+output "result_entry" {
+  value = jsondecode(data.aws_lambda_invocation.example.result)["key1"]
+}
+
+
+
 # resource "aws_lambda_invocation" "lambda_functions_invocation" {
 #   for_each      = { for cj in local.ecr_images : cj.container_image => cj }
 #   function_name = each.value.name
